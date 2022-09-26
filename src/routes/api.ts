a9697b6fb99fd8812f73af35b6d465ae8185bff2
brunopvalim <brunopvalim@gmail.com>
2022-09-26 11:10:48 -0700

    added middleware

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import * as TodoController from '../controllers/todoController';
 import * as LoginController from '../controllers/loginController';
+import { Auth } from '../middleware/auth'
 
 const router = Router();
 
@@ -15,10 +16,10 @@ router.post('/login', LoginController.login);
 router.post('/todo', TodoController.add);
 
 // UPDATE Routes
-router.put('/todo/:id', TodoController.update);
+router.put('/todo/:id', Auth.private, TodoController.update);
 
 // DELETE Routes
-router.delete('/todo/:id', TodoController.remove);
+router.delete('/todo/:id', Auth.private, TodoController.remove);
 
 
 export default router;
